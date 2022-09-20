31f6be56d2aaa3f2d6a4cbab35d3a6c30b0559f8
oguzkoroglu <oguz.koroglu@userware.onmicrosoft.com>
2022-09-20 03:36:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using DXBlazorRichEdit;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDevExpressBlazor(opts =>
+{
+    opts.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+
+await builder.Build().RunAsync();
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# Devexpress RichTextEditor Blazor Isolated Example
+
+## Prerequisites
+
+Install Devexpress Trial to get the Blazor and Blazor RichEdit Nuget packages. https://www.devexpress.com/Products/Try/
+
+## IMPORTANT
+
+The Rich Text Editor is currently only available for the *Blazor Server﻿ hosting* model. https://docs.microsoft.com/en-us/aspnet/core/blazor/hosting-models#blazor-server
